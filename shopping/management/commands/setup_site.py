df2fa6d088e2c0ed51ca7817cd1e4caf6ddc744d
ak1922 <116603167+ak1922@users.noreply.github.com>
2026-08-11 18:49:06 -0400

    Helm charts created

--- shopping/management/commands/setup_site.py
+++ shopping/management/commands/setup_site.py
@@ -5,11 +5,11 @@ class Command(BaseCommand):
     help = 'Creates or updates the Site object for kwc4.barocay.com'
 
     def handle(self, *args, **options):
-        site_id = 2  # Often set to 1 for the default site
+        site_id = 2
         domain = 'kwc4.barocay.com'
         name = 'KWC4 Barocay'
 
-        # Using get_or_create ensures no duplicate error if the site exists
+        # Use get or create to avoid duplicates
         site, created = Site.objects.update_or_create(
             id=site_id,
             defaults={'domain': domain, 'name': name}
